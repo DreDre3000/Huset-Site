05a1bcd5e6218d44fb184a5fdaa07fc28d283cd2
Andre Harris <andreharris@Andres-MacBook-Pro-2-2018.local>
2019-11-12 14:08:48 +0100

    nothing major

--- MovieInfo.js
+++ MovieInfo.js
@@ -13,3 +13,38 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+
+
+window.addEventListener("DOMContentLoaded", getData);
+
+function getData() {
+    console.log("getData")
+    //fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/posts?per_page=2&page=1")
+    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/films?_embed")
+        .then(res => res.json())
+        .then(handleData)
+}
+
+function handleData(myData) {
+    // console.log(myData);
+    // 1 loop
+    myData.forEach(showPost)
+}
+
+function showPost(post) {
+    console.log(post)
+    //2 cloning a template
+    const imgPath = post._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
+    const template = document.querySelector(".postTemplate").content;
+    const postCopy = template.cloneNode(true);
+
+    // Venue
+    const venue = postCopy.querySelector(".venue")
+    venue.innerHTML=post.venue;
+
+    //4 append
+    document.querySelector("#posts").appendChild(postCopy)
+}
+
+//Artistic Profile JS :)
+
